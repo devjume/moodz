bcb10ede4831d4d2a597d5e332097a97639c54d9
Aarne Andersson <n0anaa00@students.oamk.fi>
2023-03-27 11:34:59 +0300

    calenterikirjasto

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style/style';
 import { Text, View, StyleSheet } from "react-native";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 
 export default function CalendarScreen() {
