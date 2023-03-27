aa03bf842289655d67d641642a7960b407b46f0c
Veeti Oivo <n1oive00@students.oamk.fi>
2023-03-27 12:01:33 +0300

    SelectDropdown ladattu

--- screens/TrackerScreen.js
+++ screens/TrackerScreen.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View, StyleSheet } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
 
 
 export default function TrackerScreen() {
+
+  const [activity, setActivity] = useState("");
+  const activities = ["Sleep", "Exercise", "Relax"];
+
   return (
     <View style={styles.container}>
       <Text>Tracker</Text>
+      <SelectDropdown
+        data={activities}
+        defaultValueByIndex={0}
+        onSelect={a => setActivity}
+        >
+        <Text>{activity}</Text>
+      </SelectDropdown>
     </View>
   )
 }
