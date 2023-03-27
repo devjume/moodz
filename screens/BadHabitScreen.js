4d4b1f986ed586d24f4e6aac911d55f3afc01633
SAKU PENTTILÄ <sakuinenpenttila@gmail.com>
2023-03-27 14:14:04 +0300

    Mua harmittaa

--- screens/BadHabitScreen.js
+++ screens/BadHabitScreen.js
@@ -1,13 +1,40 @@
 import React from 'react'
-import styles from '../style/style';
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, Pressable } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function BadHabitScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Bad Habit</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.row}>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.textAlign}>Add new habit.          <AntDesign name="pluscircle" size={24} color="black" /></Text>
+        </Pressable>
+      </View>
+      
+    </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pressable: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "aquamarine"
+  },
+  row: {
+    flexDirection: 'row',
+    top: 0,
+    position: "absolute"
+  },
+  textAlign: {
+    textAlign: "center",
+    fontSize: 24
+  }
+});
