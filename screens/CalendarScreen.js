c7b3e2b041aba052b62e57fb5a4561d78645b52d
Aarne Andersson <n0anaa00@students.oamk.fi>
2023-03-28 15:12:44 +0300

    calenterlist

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,51 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import styles from '../style/style';
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View,} from "react-native";
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 
 export default function CalendarScreen() {
+
+ ///  const sleep = [sleep, setSleep] = useState([]);
+ //  const exercise = [exercise, setExercise] = useState([]);
+  // const relax = [relax, setRelax] = useState([]);
+
+
   return (
+
+
+   
     <View style={styles.container}> 
+    
       <Text>Calendar</Text>
 
-      <Calendar
+      
 
-     initialDate={'2023-03-01'}
-     minDate={'2023-05-10'}
-     maxDate={'2050-05-30'}
-     onDayPress={day => {
-     console.log('selected day', day);
-     }}
-     onDayLongPress={day => {
-      console.log('selected day', day);
-     }}
-     monthFormat={'yyyy MM'}
-     onMonthChange={month => {
-      console.log('month changed', month);
-     }}
-      hideArrows={true}
-     renderArrow={direction => <Arrow />}
-     hideExtraDays={true}
-     disableMonthChange={true}
-     firstDay={1}
-     hideDayNames={true}
-     showWeekNumbers={true}
-     onPressArrowLeft={subtractMonth => subtractMonth()}
-     onPressArrowRight={addMonth => addMonth()}
-     disableArrowLeft={true}
-     disableArrowRight={true}
-     disableAllTouchEventsForDisabledDays={true}
-     renderHeader={date => {
-      }}
-      enableSwipeMonths={true}
-    />
+   <CalendarList
+
+ ///  markingType={'multi-dot'}
+ ///  markedDates={{
+ // '2023-10-25': {dots: [relax, sleep, exercise], selected: true, selectedColor: 'red'},
+ // '2023-10-26': {dots: [sleep, exercise], disabled: true}
+  // }}
+   />
 
 
-<CalendarList
 
-
-/>
     </View>
   )
 }
