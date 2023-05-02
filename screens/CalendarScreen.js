3daee8ddf80611c56108f9f1154127a2484a4f4b
Aarne Andersson <n0anaa00@students.oamk.fi>
2023-05-02 14:01:10 +0300

    keskeneräne calender koska ios ei toimi

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -26,13 +26,15 @@ export default function CalendarScreen() {
   const [relaxValue, setRelaxValue] = useState(0)
 	const [exerciseValue, setExerciseValue] = useState(0)
 	const [sleepValue, setSleepValue] = useState(0)
-  const [dailyMood, setDailyMood] = useState(0)
+  
 
   const [relaxGoal, setRelaxGoal] = useState(0)
 	const [exerciseGoal, setExerciseGoal] = useState(0) 
 	const [sleepGoal, setSleepGoal] = useState(0)
 
   const [notes, setNotes] = useState('');
+  const [dailyMood, setDailyMood] = useState(0)
+  const [moodText, setMoodText] = useState('')
 
   const [modalVisible, setModalVisible] = useState(false);
   const [overallProgress, setOverallProgress] = useState(0);
@@ -43,7 +45,9 @@ export default function CalendarScreen() {
 
 		getUserGoals()
     getUserNotes()
+    getDateFromDailyTrack()
     getDailyMood()
+
 	}, [])
 
 
@@ -66,6 +70,8 @@ export default function CalendarScreen() {
 
 					Alert.alert("Feth daily_track error", JSON.stringify(error))
 					console.log("Feth daily_track error", error)
+
+
 					setDailyData(null)
 				}
 
@@ -74,6 +80,7 @@ export default function CalendarScreen() {
 					setExerciseValue(0)
 					setRelaxValue(0)
 					calculateOverallProgress(0,0,0)
+          
 				} else {
 
 
@@ -82,6 +89,7 @@ export default function CalendarScreen() {
 					let relaxMin = 0
 
 					daily_track[0].category_track.forEach(element => {
+
 						
             switch(element.category_id) {
 							case 1:
@@ -119,7 +127,8 @@ export default function CalendarScreen() {
 		}
 
 		function calculateSingleSection(minutes, goal) {
-			const percentage = minutes / goal
+			
+      const percentage = minutes / goal
 
 			if (percentage > 1) {
 				return 33.33
@@ -131,6 +140,7 @@ export default function CalendarScreen() {
 		const sleepSection = calculateSingleSection(sleepMin, sleepGoal)
 		const exerciseSection = calculateSingleSection(exerciseMin, exerciseGoal)
 		const relaxSection = calculateSingleSection(relaxMin, relaxGoal)
+    
 
 		const progress = sleepSection + exerciseSection + relaxSection;
 
@@ -141,7 +151,9 @@ export default function CalendarScreen() {
 
   
 	async function getUserGoals() {
+
 		let { data: profiles, error } = await supabase
+
   		.from('profiles')
   		.select('sleep_goal, exercise_goal, relax_goal')
 		
@@ -160,21 +172,153 @@ export default function CalendarScreen() {
 
 
 
+
+ ///fetch notes
+
+  async function fetchDailyId() {
+    const modifiedDate = new Date(date)
+
+    const year = modifiedDate.getFullYear();
+    const month = modifiedDate.getMonth() + 1;
+    const day = modifiedDate.getDate();
+    const wholeDate = `${year}-${month}-${day}`
+
+
+    console.log(wholeDate)
+
+    try {
+      let { data, error } = await supabase
+        .from('daily_track')
+        .select("*")
+        .eq('date', wholeDate)
+
+      console.log("fetchDailyId data: ", data) 
+      console.log("fetchDailyId error: ", error)
+      if (data[0].mood !== undefined) {
+        setNotes(data[0].mood)
+      }
+      else {
+
+        setNotes("")
+      }
+      if (data.length === 0) {
+        return undefined
+      } else {
+        return data[0].id
+      }
+    } catch (error) {
+      console.log("fetchDailyId error", error)
+      return undefined
+    }
+
+  }
+
+  async function insertCategoryTrack(idAndCategory = undefined, dailyId) {
+    try {
+
+      let hoursToMinutes = (parseInt(hours) || 0) * 60
+      let totalMinutes = hoursToMinutes + (parseInt(minutes) || 0)
+
+
+      const { data, error } = await supabase
+        .from('category_track')
+        .upsert([
+          { id: idAndCategory, category_id: activity, minutes: totalMinutes, note: notes, daily_id: dailyId, user_id: userID }
+        ])
+
+    }
+    catch (error) {
+      console.log("insertCategoryTrack error", error)
+    }
+
+  }
+
+  async function fetchIdAndCategory(dailyId) {
+    if (dailyId !== undefined) { 
+      try {
+        let { data, error } = await supabase
+          .from('category_track')
+          .select("*")
+          .eq('daily_id', dailyId)
+          .eq('category_id', parseInt(activity))
+
+        console.log("fetchIdAndCategory data", data)
+        console.log("fetchIdAndCategory error", error)
+        return data[0].id
+      }
+      catch (error) {
+        console.log("fetchIdAndCategory error", error) 
+
+        
+      }
+    } else {
+      return undefined
+    }
+  }
+
+
+  async function insertDailyAndCategory() {
+    try {
+      let dailyId = await fetchDailyId()
+      if (dailyId === undefined) {
+        dailyId = await insertDailyTrack(dailyId)
+        insertCategoryTrack(undefined, dailyId)
+      } else {
+        console.log(dailyId)
+        const idAndCategory = await fetchIdAndCategory(dailyId)
+        insertCategoryTrack(idAndCategory, dailyId)
+      }
+
+    } catch (error) {
+      console.log("insertDailyAndCategory error", error)
+    }
+
+  }
+
+
+  async function getDateFromDailyTrack() {
+    let { data: daily_track, error } = await supabase
+     .from('daily_track')
+     .select('date, category_track(categorty_id, daily_id, user_id)')
+      
+
+      if (error) {
+        console.log("Couldn't fetch profile notes")
+      }
+      else if (data.length > 0) {
+     
+        setNotes(daily_track[0].note);
+      }
+
+  }
+
+
   async function getUserNotes() {
     let { data: category_track, error } = await supabase
       .from('category_track')
       .select('note')
-      .eq('date', new Date().toISOString().slice(0, 10));
+      .eq('daily_id'); 
+
+      
 
       if (error) {
-        console-log("Couldn't fetch profile notes")
+        console.log("Couldn't fetch profile notes")
       }
       else if (data.length > 0) {
-         setNotes(category_track[0].note);
+
+      setNotes(category_track[0])
+        
+
     }
   
 
   }
+
+
+
+
+
+  // get mood
 
   async function getDailyMood() {
       let { data: daily_track, error } = await supabase
@@ -182,19 +326,45 @@ export default function CalendarScreen() {
       .select('mood')
 
       if (error) {
-        console-log("Couldn't fetch Daily Mood")
-      } else setDailyMood(daily_track[0])
-
-      displayDailyMood(mood)
-
-
-    
+        console.log("Couldn't fetch Daily Mood")
+      } else {
+        console.log(daily_track)
+        setDailyMood(daily_track[0])
+      }
   }
 
+  function MoodIcon({dailyMood}) {
+    let moodText
+    // const moodText = this.state.moodText;
+    switch (dailyMood){
+    
+      case (dailyMood === 1):
+        moodText = "dizzy";
+        break;
+      case (dailyMood === 2):
+        moodText = "frown";
+        break;
+      case (dailyMood === 3):
+        moodText = "meh";
+        break;
+      case (dailyMood === 4):
+        moodText = "smile";
+        break;
+      case (dailyMood === 5):
+        moodText = "smile-beam";
+        break;
 
+      } 
+      return <Text><FontAwesome5 name={moodText}  size={60} color="#ffe62a"/></Text>
+
+  } 
+
+
+  // calculate progress
 
 	function calculateProgress(activity) {
 		let progress = 0;
+    
 		switch(activity) {
 
 			case "sleep":
@@ -209,12 +379,17 @@ export default function CalendarScreen() {
 
       
 		}
+   
 
 		if (!isFinite(progress)) {
 			return 0
 		} else {
 			return progress
+      
 		}
+    
+
+    
 
 	}
 
@@ -225,6 +400,8 @@ export default function CalendarScreen() {
 
     setSelectedDate(day.dateString);
     setModalVisible(true);
+
+   
     
   };
 
@@ -232,6 +409,8 @@ export default function CalendarScreen() {
 
 	function CustomBar({title, progress, color}) { 
 		return (
+
+
 			<View>
 				<Text style={{fontWeight: "bold", fontSize: 18 , paddingBottom: 4}}>{title}</Text>
 				<View style={{display: "flex", flexDirection: "row", alignItems: "center", gap: 8, justifyContent: "space-between"}}>
@@ -241,32 +420,28 @@ export default function CalendarScreen() {
 			</View>
 		)
 	} 
+
+  useEffect(() => {
+
+    console.log("Modal avattu")
+
+  }, [selectedDate])
+
+function RenderModal({selectedDate}) {
   
-
-
-  const renderModal = () => {
-    const event = data[selectedDate] || {};
 
     return (
       <Modal  visible={modalVisible} animationType="slide">
-    {/*     <View style={styles.container}>
-          <Text style={styles.dayStatHeader}>Day Statistics</Text>
-          <Text>Date: {selectedDate}</Text> 
-          <Text>Sleep: {event.sleep || '-'}</Text>
-          <Text>Exercise: {event.exercise || '-'}</Text>
-          <Text>Relax: {event.relax || '-'}</Text>
-          <TouchableOpacity onPress={() => setModalVisible (false)}>
-            <Text style={styles.buttonText}>Close</Text>
-          </TouchableOpacity>
-        </View> */}
       <ScrollView>
       <View style={styles.container}>
         <Text style={styles.dayStatHeader}>Day Statistics</Text>
 				<View style={{display: "flex", flexDirection: "row", gap: 90, alignItems: "center"}}>
 					<Text style={styles.title}>{selectedDate}</Text>
+          
 				</View>
         
         <View style={{display: "flex", flexDirection: "row", gap: 90, alignItems: "center"}}>
+
           <View>
            <CircularProgress 
               size={130}
@@ -275,7 +450,7 @@ export default function CalendarScreen() {
               tintColor="#498467"
                 arcSweepAngle={270}
                 rotation={225}
-                backgroundColor="#D9D9D9"
+                backgroundColor="#D9D9D9"      
                 lineCap='round' 
                 style={{marginVertical: 10}}
                 children={(e) => <Text style={{fontWeight: "bold", fontSize: 28}}>{e}</Text>}
@@ -286,10 +461,11 @@ export default function CalendarScreen() {
             </View>  
 
             <View>
-
-      
-              <FontAwesome5 name="smile-beam" size={70} color={dailyMood === 5 ? "#000000" : "#ffe62a"} />
-              <Text style={styles.subHeader}>Daily Mood</Text>
+              <MoodIcon dailyMood={dailyMood}/>
+              
+ 
+                
+              <Text style={styles.subHeader}>Daily Mood</Text> 
 
             </View> 
 
@@ -300,11 +476,12 @@ export default function CalendarScreen() {
 
         <Text style={styles.dayStatHeader}>Sleep Notes</Text>
           <TextInput
-             style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
+              progress={insertDailyAndCategory()}
+              style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
               multiline={true}
               editable = {false}
               value={notes}
-               onChangeText={setNotes}
+               
         />
 
 
@@ -313,11 +490,12 @@ export default function CalendarScreen() {
 
         <Text style={styles.dayStatHeader}>Exercise Notes</Text>
           <TextInput 
-             style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
+              progress={insertDailyAndCategory()}
+              style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
               multiline={true}
               editable = {false}
               value={notes}
-               onChangeText={setNotes}
+               
         />
 
 
@@ -326,21 +504,23 @@ export default function CalendarScreen() {
 
         <Text style={styles.dayStatHeader}>Relax Notes</Text>
           <TextInput
-             style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
+              progress={insertDailyAndCategory()}
+              style={{ height: 200, width: '80%', borderColor: 'gray', borderWidth: 1, marginTop: 20, marginBottom: 25 }}
               multiline={true}
               editable = {false}
               value={notes}
-               onChangeText={setNotes} 
+            
           />
 
-        <TouchableOpacity onPress={() => setModalVisible (false)} style={styles.button}>
-            <Text style={styles.buttonText}>Close</Text>
-          </TouchableOpacity>
+        <Pressable style={styles.button} onPress={() => setModalVisible (false)} >
+            <Text style={styles.buttonText2}>Close</Text>
+          </Pressable>
 			</View>
       </ScrollView>
       </Modal>
     );
   };
+
 
   return (
     <View>
@@ -350,7 +530,7 @@ export default function CalendarScreen() {
             pastScrollRange={6}
             futureScrollRange={1}
             scrollEnabled={true}/>
-      {selectedDate && renderModal()}
+      {selectedDate && <RenderModal selectedDate={selectedDate} />}
     </View>
   );
 }
